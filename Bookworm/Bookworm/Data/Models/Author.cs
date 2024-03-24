a0138ae761107dc0e33e05089a4f1b6d256fdc9b
Ellieto562 <146523899+Ellieto562@users.noreply.github.com>
2024-03-24 02:25:23 +0200

    added the unit tests

--- Bookworm/Bookworm/Data/Models/Author.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bookworm.Data.Models
-{
-    public class Author
-    {
-        public int AuthorId { get; set; }
-        public string Name { get; set; }
-        public ICollection<Book> Books { get; set; }
-    }
-}
